d21a3cc4ea43002d5e80f45e2899bb8b7bbc6537
valentin.kozlov <valentin.kozlov@kit.edu>
2018-10-21 00:25:41 +0200

    simplify repository structure

--- 2dsemseg/features/build_features.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Feature building
-"""
-
-# import project config.py
-import 2dsemseg.config as cfg
--- 2dsemseg/visualization/visualize.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Visualization
-"""
-# import project config.py
-import 2dsemseg.config as cfg
