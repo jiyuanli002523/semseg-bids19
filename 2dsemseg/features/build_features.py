4f431e59de5ea909c39324c83ea7a3d3d3226b01
valentin.kozlov <valentin.kozlov@kit.edu>
2018-10-20 22:26:26 +0200

    initial commit

---
+++ 2dsemseg/features/build_features.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Feature building
+"""
+
+# import project config.py
+import 2dsemseg.config as cfg
---
+++ 2dsemseg/visualization/visualize.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Visualization
+"""
+# import project config.py
+import 2dsemseg.config as cfg
