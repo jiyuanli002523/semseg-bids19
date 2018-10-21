e40138b8badab41287dcb1370482376befc89019
eo9869@kit.edu <valentin.kozlov@kit.edu>
2018-10-21 12:40:31 +0200

    simplify job_bmetal.sh and job_udocker.sh. Add comments inside

--- job_bmetal.sh
+++ job_bmetal.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 ##### INFO #####################
-# script to be submitted to the batch system as a job
+# Script to be submitted to the batch system as a job
 # bare-metal case
 ################################
 
 ####### MAIN CONFIG #######
-# NUMGPUS: number of GPUs to use, up to max number of cards available at ONE node
+# NUMGPUS: number of GPUs to use, up to the max number available on ONE node
 NUMGPUS=4
 # VIRTEnv: python virtual environment where all necessary packages are installed
 VIRTEnv="bids2019"
 source $HOME/.venv/$VIRTEnv/bin/activate
 
-DLProject=$HOME/workspace/2dsemseg                                 # directory with the source code
-DLScript=$DLProject/2dsemseg/train_resnet50_fcn.py                 # deep learning script to run
-RUNName="bmetal-$VIRTEnv-gpus=$NUMGPUS"                            # name of the run
-DLData=$HOME/datasets/vaihingen/data                               # directory with training and valiation data (.hdf5)
-DLModel=$HOME/datasets/vaihingen/models/$RUNName"_weights.hdf5"    # path to output model file with weights
-DLLog=$DLProject/reports                                           # where to store summary report
+# RUNName: name of the run
+RUNName="bmetal-$VIRTEnv-gpus=$NUMGPUS"
 
-DLScriptOpts="--data_path=$DLData --model=$DLModel --log=$DLLog/$RUNName-log.csv --n_gpus=$NUMGPUS --n_epochs=20"
+DLProject=$HOME/workspace/2dsemseg                     # directory with the source code
+DLScript=$DLProject/2dsemseg/train_resnet50_fcn.py     # deep learning script to run
+DLData=$HOME/datasets/vaihingen/data                   # directory with Training and Valiation data (.hdf5)
+DLModels=$HOME/datasets/vaihingen/models               # directory for the output model file with weights
+
+# DLScriptOpts: options for the script. N.B. default n_epochs is 20
+DLScriptOpts="--data_path=$DLData --model=$DLModels/$RUNName"_weights.hdf5" --log=$DLModels/$RUNName-log.csv --n_gpus=$NUMGPUS --n_epochs=2"
 ########
 
 ## for NFS v2/v3 local cache path needed
 export CUDA_CACHE_PATH=/tmp
-#
 ## libraries for cuda
 export CUDASYS='/usr/local/cuda'
 export PATH=$CUDASYS'/bin':$PATH
--- job_udocker.sh
+++ job_udocker.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 ##### INFO ######
-# This script supposes to:
-# 1. run 2dsemseg2019 code inside the container by means of udocker
-#
-# VKozlov @20-Oct-2018
+# Script to run container with 2dsemseg code by means of udocker
 #
 # udocker: https://github.com/indigo-dc/udocker
 #
+# VKozlov @20-Oct-2018
 ################
 
 ####### MAIN CONFIG #######
-UCONTAINER="bids2019-gpu"                             # container to use
-NUMGPUS=4                                             # in some systems a node has >1 GPU. e.g. in LSDF one can set NUMGPUS=4 (max)
-HOSTData=$HOME/datasets/vaihingen/data
-HOSTModels=$HOME/datasets/vaihingen/models
+# NUMGPUS: number of GPUs to use, up to the max number available on ONE node
+NUMGPUS=4
+# UCONTAINER: udocker container to use
+# Example to prepare the container:
+# $ udocker pull vykozlov/2dsemseg:bids2019-gpu
+# $ udocker create --name=bids2019-gpu vykozlov/2dsemseg:bids2019-gpu
+UCONTAINER="bids2019-gpu"
 
-DLScript="/2dsemseg/2dsemseg/train_resnet50_fcn.py"   # Deep learning script to run (path inside container)
+# RUNName: name of the run
 RUNName="udocker-gpus=$NUMGPUS"
-DLData="/2dsemseg/data"
-DLModel=/2desemseg/models/$RUNName"_weights.hdf5"
-DLCsvLog=/2dsemseg/models/$RUNName"-log.csv"
 
-DLScriptOpts="--data_path=$DLData --model=$DLModel --log=$DLCsvLog --n_gpus=$NUMGPUS"      # options for the script
+HOSTData=$HOME/datasets/vaihingen/data                # HOST directory with Training and Validation data
+HOSTModels=$HOME/datasets/vaihingen/models            # HOST directory for the output model with weights
 
-#--------------------------
-UDOCKER_DIR="$HOME/.udocker"                          # udocker main directory.
+DLScript="/2dsemseg/2dsemseg/train_resnet50_fcn.py"   # deep learning script to run (path inside container. fixed in the container)
+DLData=/2dsemseg/data                                 # mount point inside container for Training and Validation data
+DLModels=/2desemseg/models                            # mount point inside container for the output model
+
+# DLScriptOpts: options for the script. N.B. default n_epochs=20
+DLScriptOpts="--data_path=$DLData --model=$DLModels/$RUNName"_weights.hdf5" --log=$DLModels/$RUNName"-log.csv" --n_gpus=$NUMGPUS"
 ###########################
-echo "==================================="
-echo "=> udocker container: $UCONTAINER"
-echo "==================================="
-### following line has to be run only first time
+UDOCKER_DIR="$HOME/.udocker"                          # udocker main directory.
+echo "[INFO] udocker container: $UCONTAINER"
+# following line has to be run only first time
 udocker setup --nvidia ${UCONTAINER}
 ##### >>> now run the script:
-udocker run -v $HOSTData:/2dsemseg/data -v $HOSTModels:/2dsemseg/models ${UCONTAINER} python $DLScript $DLScriptOpts
+# we mount HOST directory with Training and Validation data 
+# and directory for the output model inside the container
+udocker run -v $HOSTData:$DLData -v $HOSTModels:$DLModels ${UCONTAINER} python $DLScript $DLScriptOpts
